d34ee9c51b513ab0f993a4927eeb21ab0717d010
MRWinkel <80113588+MRWinkel@users.noreply.github.com>
2021-03-05 17:04:38 +0100

    excercise 2: printing 3 strings

---
+++ part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -0,0 +1,14 @@
+
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+        /* Modify the program so that it will print the following text. 
+         * Use three print commands for printing.
+         * Once upon a time
+         * there was
+         * a program */
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+}
