bfb2ea504f64cc6818299872069de415fbc0a953
MRWinkel <80113588+MRWinkel@users.noreply.github.com>
2021-03-05 16:43:59 +0100

    Add files via upload
    Exercise 1: printing a String

---
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,9 @@
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Write your program here
+        //Add a new line below the line comment that prints the string "Ada Lovelace" and run the program.
+        System.out.println("Ada Lovelace");
+    }
+}
